2e503afacdcad06b7cc145300b947135092ad4a3
Matheca123 <mateuscorreia14@gmail.com>
2026-03-28 00:11:33 -0300

    Upd das perguntas

--- arquivosjs/script.js
+++ arquivosjs/script.js
@@ -1,18 +1,64 @@
 const perguntas = [
   {
-    pergunta: "Qual é o planeta mais próximo do Sol?",
-    opcoes: ["Vênus", "Marte", "Mercúrio", "Júpiter"],
-    correta: 2,
-  },
-  {
-    pergunta: "Quanto é 2 + 2?",
-    opcoes: ["3", "4", "5", "6"],
+    pergunta:
+      "Em programação, uma sequência de instruções executadas na ordem em que aparecem é chamada de:",
+    opcoes: ["Repetição", "Sequência", "Condição", "Variável"],
     correta: 1,
   },
   {
-    pergunta: "Qual a capital do Brasil?",
-    opcoes: ["Rio de Janeiro", "São Paulo", "Brasília", "Salvador"],
+    pergunta: "Quais das alternativas não se encaixa como proposição lógica?",
+    opcoes: [
+      "2+2=45",
+      "A Inglaterra é um país",
+      "Gosto do churrasco",
+      "Conte até 32",
+    ],
+    correta: 3,
+  },
+  {
+    pergunta:
+      "Considere o algoritmo: se número > 10 então mostrar 'Grande', senão mostrar 'Pequeno'. Se número = 10, o programa mostrará:",
+    opcoes: ["Grande", "Pequeno", "Erro", "Nada"],
+    correta: 1,
+  },
+  {
+    pergunta: "Determine o valor de X: x = 5 | x = x + 3 | mostrar x",
+    opcoes: ["3", "5", "8", "15"],
     correta: 2,
+  },
+  {
+    pergunta:
+      "Qual é o principal problema no algoritmo: x = 10 | se x > 5 mostrar 'A' senão mostrar 'B'?",
+    opcoes: [
+      "Falta identação/bloco",
+      "A variável não existe",
+      "O número é inválido",
+      "O programa está correto",
+    ],
+    correta: 0,
+  },
+  {
+    pergunta:
+      "Um programa deve mostrar 'Aprovado' apenas se: nota ≥ 7 e frequência ≥ 75%. Qual condição correta?",
+    opcoes: [
+      "nota >= 7 OU frequencia >= 75",
+      "nota >= 7 E frequencia >= 75",
+      "nota > 7 OU frequencia > 75",
+      "nota >= 7",
+    ],
+    correta: 1,
+  },
+  {
+    pergunta:
+      "Observe o algoritmo: x = 2 | enquanto x < 10 { x = x * 2; mostrar x } Quais números podem ser exibidos?",
+    opcoes: ["4 8 16", "2 4 8", "4 8", "2 4 8 16"],
+    correta: 0,
+  },
+  {
+    pergunta:
+      "Considere o algoritmo: x = 2, y = 3, z = 0 | enquanto x < 10 { z = z + y; x = x + 2; y = y + 1 } mostrar z. Qual o valor final?",
+    opcoes: ["15", "18", "21", "25"],
+    correta: 1,
   },
 ];
 
